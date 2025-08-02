9a447c84427482b0867d33c464ad024ad3483451
Joseph Siwiti <josephsiwiti@gmail.com>
2025-08-01 17:57:16 -0700

    added a script for displaying languages method

---
+++ 1-multi_languages.js
@@ -0,0 +1,8 @@
+function greetInMultipleLanguages() {
+    console.log("C is fun");
+    console.log("Python is cool");
+    console.log("JavaScript is amazing");
+    
+}
+
+greetInMultipleLanguages()
