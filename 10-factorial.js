a1d9897c3e2fffdf6c57a17a3f100f0221b1f780
Joseph Siwiti <josephsiwiti@gmail.com>
2025-08-03 11:18:27 -0700

    prints factorial

---
+++ 10-factorial.js
@@ -0,0 +1,12 @@
+function theFactorial(){
+    let input = parseInt(process.argv[2]);
+
+function thefactorial(n) {
+  if (isNaN(n)) return 1;
+  if (n <= 1) return 1;
+  return n * thefactorial(n - 1);
+}
+
+console.log(thefactorial(input));
+
+}
