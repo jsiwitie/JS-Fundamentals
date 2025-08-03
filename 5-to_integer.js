6b3fc99ec2eaca8a0ac2c8e2186d6d7d99378910
Joseph Siwiti <josephsiwiti@gmail.com>
2025-08-03 00:55:48 -0700

    prints an integer

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,6 +1,15 @@
 function myNumber(){
 
-    let argument = process.argv[2];
+
+    const arg = process.argv[2];
+const num = parseInt(arg);
+
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log("Not a number");
+}
+/*    let argument = process.argv[2];
     let number = parseInt(argument);
   
 
@@ -10,5 +19,5 @@ function myNumber(){
       console.log("Not a number");
      }
 
-}
+}*/
 myNumber();
